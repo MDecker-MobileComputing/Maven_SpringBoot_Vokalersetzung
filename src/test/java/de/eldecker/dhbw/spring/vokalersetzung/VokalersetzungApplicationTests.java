2e3dad22b1e1f0c4704ee05d940001da7a0da228
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-12-08 19:04:32 +0100

    Projekt von Spring Initializr

---
+++ src/test/java/de/eldecker/dhbw/spring/vokalersetzung/VokalersetzungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.eldecker.dhbw.spring.vokalersetzung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VokalersetzungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
