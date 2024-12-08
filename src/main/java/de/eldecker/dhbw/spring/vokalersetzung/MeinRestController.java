d16582bb2ff7af4a0888169b7ddc0d79f06c4e99
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-12-08 20:25:38 +0100

    Update MeinRestController.java

--- src/main/java/de/eldecker/dhbw/spring/vokalersetzung/MeinRestController.java
+++ src/main/java/de/eldecker/dhbw/spring/vokalersetzung/MeinRestController.java
@@ -4,12 +4,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+/**
+ * Die in dieser Klasse enthaltene Methode ist zu vervollständigen.
+ */
 @RestController
 @RequestMapping("/vokalersetzung/v1")
 public class MeinRestController {
-
     
-    public ResponseEntity<String> vokalErsetzen( String inputText, char zielvokal ) {
+    public ResponseEntity<ErgebnisRecord> vokalErsetzen( String inputText, 
+                                                         char zielvokal ) {
         
         return null;
     }
